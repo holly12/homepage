6bc69bd1487db0674f10e0dfaa260e5317c7d091
Kevin Bongart <contact@kevinbongart.net>
2013-07-16 12:29:01 -0400

    Build with relative assets

--- config.rb
+++ config.rb
@@ -60,18 +60,18 @@ configure :build do
   # activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
