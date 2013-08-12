1b71a63d24cd3a34c70ed703f67042bb4b00675e
Kevin Bongart <contact@kevinbongart.net>
2013-08-12 13:23:32 -0400

    Config

--- config.rb
+++ config.rb
@@ -1,80 +1,21 @@
-###
-# Compass
-###
+require 'zurb-foundation'
 
-# ZURB Foundation
-require "zurb-foundation"
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-page "robots.txt", :layout => false
-page "humans.txt", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-page "/25-things-i-wish-i-had-learned-earlier.html", layout: "layout"
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Livereload
-activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+page 'robots.txt', layout: false
+page 'humans.txt', layout: false
 
 set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :js_dir, 'javascripts'
 
-# Build-specific configuration
+activate :livereload
+
+compass_config do |config|
+  config.output_style = :compact
+end
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
+  require 'middleman-smusher'
   activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
   activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  require "middleman-smusher"
   activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
